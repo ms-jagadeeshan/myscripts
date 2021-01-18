bfe3686a677a52fcf174e4cf6d1876b482b747ef
JagadeeshanMS <jagadeeshanmsj@gmail.com>
2021-01-19 03:20:04 +0530

    renamed

--- automatic_class_joiner/online_class_automation.sh
+++ automatic_class_joiner/online_class_automation.sh
@@ -4,8 +4,7 @@ hour=$(date +%H)
 minute=$(date +%M)
 day=$(date +%w)
 Ec_batch=1
-dir="~/lib/sh"
-cd $dir
-g++ link_provider.cpp -o link_provider 
+dir="~/lib/sh/automatic_class_joiner"
+cd $dir g++ $dir link_provider.cpp -o link_provider 
 link=$(./link_provider)
 xdg-open $link
